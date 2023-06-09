5491e31591e00d6aee7be3177c2a6364d1186412
Junyi Xie <yunyi.xie@outlook.com>
2023-06-09 17:05:50 +0200

    DigimonOverview controller and fxml

---
+++ src/main/java/digimonbattlesimulator/controller/DigimonOverviewController.java
@@ -0,0 +1,13 @@
+package digimonbattlesimulator.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DigimonOverviewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //TODO Create overview when clicking on digimon screen
+    }
+}
