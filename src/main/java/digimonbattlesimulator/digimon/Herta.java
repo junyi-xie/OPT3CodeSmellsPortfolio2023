e8eb1c174b682fcae17ed2951c60a53a6a6a15da
Junyi Xie <yunyi.xie@outlook.com>
2023-06-11 13:23:00 +0200

    Update Herta.java

--- src/main/java/digimonbattlesimulator/digimon/Herta.java
+++ src/main/java/digimonbattlesimulator/digimon/Herta.java
@@ -37,9 +37,9 @@ public class Herta extends Digimon {
             new AttackTechnique("/digimonbattlesimulator/images/37_saint_ray_icon.png", "Saint Ray", "Attack that drops a column of light on the target's head.", 80, Type.HOLY),
             new AttackTechnique("/digimonbattlesimulator/images/38_holy_breath_icon.png", "Holy Breath", "Increases the stamina and speed of allies.", 80, Type.HOLY),
             new AttackTechnique("/digimonbattlesimulator/images/39_flash_icon.png", "Flash", "Deals damage to enemies by exploding them from within with light.", 90, Type.HOLY),
-            new AttackTechnique("/digimonbattlesimulator/images/40_saint_shield_icon.png", "Saint Shield", "Deals damage to area around target with a dome of light.",110, Type.HOLY),
-            new AttackTechnique("/digimonbattlesimulator/images/41_judgement_icon.png", "Judgement", "Attacks enemies with an extra powerful laser of judgement.",150, Type.HOLY),
-            new AttackTechnique("/digimonbattlesimulator/images/42_shining_nova_icon.png", "Shining Nova", "Fills the surrounding area with light energy, dealing much damage to enemies.",150, Type.HOLY)
+            new AttackTechnique("/digimonbattlesimulator/images/40_saint_shield_icon.png", "Saint Shield", "Deals damage to area around target with a dome of light.", 110, Type.HOLY),
+            new AttackTechnique("/digimonbattlesimulator/images/41_judgement_icon.png", "Judgement", "Attacks enemies with an extra powerful laser of judgement.", 150, Type.HOLY),
+            new AttackTechnique("/digimonbattlesimulator/images/42_shining_nova_icon.png", "Shining Nova", "Fills the surrounding area with light energy, dealing much damage to enemies.", 150, Type.HOLY)
         );
     }
 }
